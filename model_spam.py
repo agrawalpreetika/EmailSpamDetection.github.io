a429d314244a637f8cd44719f604dce0ac6d6c4d
Preetika Agrawal <145897932+agrawalpreetika@users.noreply.github.com>
2024-07-21 09:57:37 +0530

    Add files via upload

--- model_spam.py
+++ model_spam.py
@@ -1,27 +1,23 @@
+
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split , cross_val_score
+import sklearn
+from sklearn.model_selection import train_test_split 
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 
 
-
-data=pd.read_csv(r'spam.csv',encoding='latin-1')
-
+data=pd.read_csv(r'/Users/utkarsh/Python and ML/spam.csv',encoding='latin-1')
 
 data.drop(['Unnamed: 2','Unnamed: 3','Unnamed: 4'],axis=1,inplace=True)
 
-
 data.rename(columns={'v1':'Category','v2':'Email'},inplace=True)
-
 
 data=data.drop_duplicates()
 
-
 label_encoder=LabelEncoder() 
 data['Category']=label_encoder.fit_transform(data['Category'])
-
-
 
 import nltk
 from nltk.corpus import stopwords
@@ -38,46 +34,35 @@ def preprocess_text(text):
     if not isinstance(text, str):
         return ''
 
-    
     text=text.lower()
 
-    
-    text=re.sub(r"[^a-zA-Z\s]","",text)
+    text=re.sub(r"[^a-zA-Z0-9\s]","",text)
     
     text=word_tokenize(text)
 
-    
     stop_words=set(stopwords.words('English')) 
     text=[word for word in text if word not in stop_words]
-    
-
     
     lemmatizer=WordNetLemmatizer() 
     text=[lemmatizer.lemmatize(word) for word in text]
 
-    
     text=' '.join(text)
+    
     return text
 
 data['Email']=data['Email'].apply(preprocess_text)
 
-
 from sklearn.feature_extraction.text import CountVectorizer
-
 vectorizer=CountVectorizer()
-
 
 x=vectorizer.fit_transform(data['Email'])
 y=data['Category']
 
-
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
 
-clf=LogisticRegression()
-clf.fit(x_train,y_train)
+model=LogisticRegression()
+model.fit(x_train,y_train)
 
 import pickle
-pickle.dump(clf,open('model.pkl','wb'))
-
-with open('vectorizer.pkl', 'wb') as f:
-    pickle.dump(vectorizer, f)
+pickle.dump(model,open('model.pkl','wb'))
+pickle.dump(vectorizer,open('vectorizer.pkl','wb'))
